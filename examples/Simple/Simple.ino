eed34d4e118c5093729d376326964e6b3097fa84
Christian Scott <christian@kreativitet.no>
2016-11-08 10:03:03 +0100

    Adds non-blocking functionality

--- examples/Simple/Simple.ino
+++ examples/Simple/Simple.ino
@@ -4,22 +4,17 @@
 const unsigned int stepCount = 4096;
 const double speed = 5;
 
-// Use pins 6-9 or 10-13 on the MEGA2560 for a slightly optimized version
 Stepper sOptimal1(Stepper::PinGroup::MEGA2560_10_13);
 Stepper sOptimal2(Stepper::PinGroup::MEGA2560_6_9);
-// Otherwise use this generic version which isn't exactly a slouch either
 Stepper sSlow(10, 11, 12, 13);
 
 void setup() {
-  // Speed is in RPM
   sOptimal1.setSpeed(speed);
   sSlow.setSpeed(speed);
 }
 
 void loop() {
-  // Positive step counts go counter-clockwise
   sOptimal1.step(stepCount);
-  // Negative step counts go clockwise
   delay(500);
   sOptimal1.step(-stepCount);
   delay(500);
