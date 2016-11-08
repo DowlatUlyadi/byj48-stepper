05f6061a6a528cf184ca00ed032764c6e16cb559
Christian Scott <christian@kreativitet.no>
2016-11-08 01:49:21 +0100

    Fixes simple example

--- examples/Simple/Simple.ino
+++ examples/Simple/Simple.ino
@@ -1,22 +1,25 @@
-#include "Stepper.h"
+#include "BYJ48Stepper.h"
 
 // 4096 steps per rotation on the BYJ
 const unsigned int stepCount = 4096;
 const double speed = 5;
 
+// Use pins 6-9 or 10-13 on the MEGA2560 for a slightly optimized version
 Stepper sOptimal1(Stepper::PinGroup::MEGA2560_10_13);
 Stepper sOptimal2(Stepper::PinGroup::MEGA2560_6_9);
+// Otherwise use this generic version which isn't exactly a slouch either
 Stepper sSlow(10, 11, 12, 13);
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  // Speed is in RPM
   sOptimal1.setSpeed(speed);
   sSlow.setSpeed(speed);
 }
 
 void loop() {
+  // Positive step counts go counter-clockwise
   sOptimal1.step(stepCount);
+  // Negative step counts go clockwise
   delay(500);
   sOptimal1.step(-stepCount);
   delay(500);
